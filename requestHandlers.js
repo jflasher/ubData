2baae2c542e49455dd637f961ad2eb4e86647889
jflasher <joseph.flasher@gmail.com>
2012-11-03 22:02:09 +0800

    Changing Mongolian tweet text.

--- requestHandlers.js
+++ requestHandlers.js
@@ -180,11 +180,10 @@ function sendTweet(response, request) {
 			var textEN = "PM2.5 = " + pm25 + "\u00B5g/m\u00B3, this is " + m + "X the WHO 24hr standard. Reporting 24hr average from " + pm25Arr.length + " of " + stations.length + " stations as of " + dayStringEN + " " + curHour + ":" + curMin + ". #UBAir";
 			
 			// Mongolian string
-			var textMN = "PM2.5 = " + pm25 + "\u00B5g/m\u00B3, энэ нь " + m + "X буюу WHO 24 цагийн стандарт. " + dayStringMN + " ны " + curHour + ":" + curMin + " цагийн байдлаар 24 цагийн дундажтай " + stations.length + " станцын " + pm25Arr.length + " ээс мэдээлэв.";
+			var textMN = "PM2.5 = " + pm25 + "\u00B5g/m\u00B3. ДЭМБ 24 цагийн стандартай харицуулхад " + m + "X. " + dayStringMN + ", " + curHour + ":" + curMin + " цагийн байдлаар 24 цагийн дунджаар " + stations.length + " станцын " + pm25Arr.length + " ээс мэдээлэв.";
 			
-			
-			console.log(textEN);
-			console.log(textMN);
+			//console.log(textEN);
+			//console.log(textMN);
 			//console.log(textEN.length);
 			//console.log(textMN.length);
 			
