65ac2dbe1bc23320047ce566eff8b1ea61e0bac4
jflasher <joseph.flasher@gmail.com>
2013-05-02 22:18:48 -0400

    Beginning of a nicer info page

--- router.js
+++ router.js
@@ -1,18 +1,7 @@
 var requestHandlers = require('./requestHandlers');
 var keys = require('./twitterkeys');
 
-var routes = function (app) {
-  app.get('/', function (req, res) {
-    res.render('index', function (err, html) {
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        res.end(html);
-      }
-    });
-  });
-
+var routes = function (app) { 
   app.get('/1/mostRecent', function (req, res) {
     requestHandlers.getMostRecent(req, res);
   });
