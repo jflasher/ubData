71f3d2d570758760b831031bbfbc4fead9d7d55d
Joseph Flasher <flasher@istrategylabs.com>
2013-05-04 15:42:02 -0400

    Almost final changes to the new webpage

--- router.js
+++ router.js
@@ -1,31 +1,9 @@
 var requestHandlers = require('./requestHandlers');
 
 var routes = function (app) {
-  app.get('/', function (req, res) {
-    res.render('index', function (err, html) {
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        res.end(html);
-      }
-    });
-  });
-
   ////
   //// Version 1
   ////
-  app.get('/1/showInfo', function (req, res) {
-    res.render('index', function (err, html) {
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        res.end(html);
-      }
-    });
-  });  
-
   app.get('/1/mostRecent', function (req, res) {
     requestHandlers.getMostRecent(req, res);
   });
